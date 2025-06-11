ab15393740d92d9b38734f8cd00f850dd2804eaa
KevinZonda <33132228+KevinZonda@users.noreply.github.com>
2025-06-11 14:04:39 +0800

    fix

--- src/main/main.go
+++ src/main/main.go
@@ -9,17 +9,17 @@ import (
 // main 函数：程序入口
 // 创建任务链表并添加示例任务
 func main() {
-	TaskList := &calculation.TaskList{Head: nil}
+	taskList := &calculation.TaskList{Head: nil}
 
-	TaskList.AddTask("坠机", "00:10", "00:30")
-	TaskList.AddTask("MAN", "00:10", "00:40")
-	TaskList.AddTask("打德佬", "12:00", "13:50")
-	TaskList.AddTask("OFN启动", "12:00", "13:50")
-	TaskList.AddTask("黑联启动", "12:00", "13:50")
+	taskList.AddTask("坠机", "00:10", "00:30")
+	taskList.AddTask("MAN", "00:10", "00:40")
+	taskList.AddTask("打德佬", "12:00", "13:50")
+	taskList.AddTask("OFN启动", "12:00", "13:50")
+	taskList.AddTask("黑联启动", "12:00", "13:50")
 
-	Content := TaskList.TaskTimeLine("00:00", 10, 0, "15:04")
+	content := taskList.TaskTimeLine("00:00", 10, 0, "15:04")
 	fmt.Println("--------------------")
-	timeIO.WriteFile("./file.txt", Content)
+	timeIO.WriteFile("./file.txt", content)
 
 	// // 打印从00:00开始，每10分钟一个时间点，打印6个时间点
 	// printTimeSequence("00:00", 10, 10)
